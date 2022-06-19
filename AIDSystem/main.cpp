99f4c18328a5b19b0d581260086e05db5777394f
Xewol <bartekdeluga0702@gmail.com>
2022-06-19 13:24:07 +0200

    naprawa

--- AIDSystem/main.cpp
+++ AIDSystem/main.cpp
@@ -10,14 +10,6 @@ int main()
     for (const auto& i : SZP1.m_pacjenci) {
         std::cout << i << "\n\n";
 
+    }
 
-int main()
-{
-    
-    Pacjent Bartek(69, "Bartosz", "Deluga", "12345678910", 6894, 12,
-        "Staromlynska", 21, 2, "66-530", "Szczecin", "Polska");
-    std::cout << Bartek << std::endl;
-    
-   
-    
 }
