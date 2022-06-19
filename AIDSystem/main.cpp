2e12a04909e4fce2e6e7b7ad340b93a77aad58f0
Maciej <76630179+HAHAHAHAHAXDDD@users.noreply.github.com>
2022-06-19 13:06:26 +0200

    main

--- AIDSystem/main.cpp
+++ AIDSystem/main.cpp
@@ -9,5 +9,7 @@ int main()
 {
     Pacjent Bartek(69, "Bartosz", "Deluga", "12345678910", 6894, 12,
         "Staromlynska", 21, 2, "66-530", "Szczecin", "Polska");
+    Pracownik Mikhyalio(420, "Mikhyalio", "Fedorov", "69696969696", "Prezes", "500000", "213769420", "01-01-1720", "Szczecinska", 69, 420, "69-420", "Szczecin", "Ukraina");
     std::cout << Bartek << std::endl;
+    std::cout << std::endl << Mikhyalio << std::endl;
 }
